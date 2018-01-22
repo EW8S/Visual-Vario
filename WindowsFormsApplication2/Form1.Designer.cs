16d3aa393e9003ad3fec0b147568332830478605
Aliaksandr Kisel <ew8s.software@gmail.com>
2018-01-22 11:42:23 +0300

    22_01_2018

---
+++ WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button2 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Открыть";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(11, 695);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(1702, 23);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 68);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(121, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Считать данные";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(139, 12);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.IsVisibleInLegend = false;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Color = System.Drawing.Color.Red;
+            series2.IsVisibleInLegend = false;
+            series2.Legend = "Legend1";
+            series2.Name = "Series2";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(1574, 491);
+            this.chart1.TabIndex = 4;
+            this.chart1.Text = "chart1";
+            this.chart1.AxisViewChanged += new System.EventHandler<System.Windows.Forms.DataVisualization.Charting.ViewEventArgs>(this.chart1_AxisViewChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 97);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(121, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Из файла";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 126);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(121, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Вывод давления";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(11, 173);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(121, 20);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(11, 250);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(121, 20);
+            this.textBox2.TabIndex = 8;
+            this.textBox2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox2_KeyDown);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 375);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(121, 20);
+            this.textBox3.TabIndex = 9;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(13, 346);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(120, 23);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "Расчет перепада";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(11, 200);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(121, 20);
+            this.textBox4.TabIndex = 12;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(13, 276);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(119, 20);
+            this.textBox5.TabIndex = 13;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(11, 428);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(121, 20);
+            this.textBox6.TabIndex = 14;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(13, 402);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(119, 20);
+            this.textBox7.TabIndex = 15;
+            // 
+            // chart2
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart2.Legends.Add(legend2);
+            this.chart2.Location = new System.Drawing.Point(139, 491);
+            this.chart2.Name = "chart2";
+            series3.ChartArea = "ChartArea1";
+            series3.IsVisibleInLegend = false;
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.chart2.Series.Add(series3);
+            this.chart2.Size = new System.Drawing.Size(1574, 198);
+            this.chart2.TabIndex = 16;
+            this.chart2.Text = "chart2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1743, 730);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+    }
+}
+
---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,343 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+using System.IO;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+
+        static bool _continue;                              //Состояние порта
+        static SerialPort _serialPort;                      //Эксземляр класса ком порт
+        static byte[] uart_mass = new byte[0x1FFFFF];       //Применый массив
+        static int uart_count;                              //Счетчик
+        static string fileName;
+
+        float varVario = 10.63f;
+        float varProcess = 0.05f;
+        float Pc = 0.0f;
+        float G = 0.0f;
+        float P = 1.0f;
+        float Xp = 0.0f;
+        float Zp = 0.0f;
+        float Xe = 0.0f;
+
+        static float ex_r, r;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //-----------------------------------------------------------  Выводим доступные COM port
+        private void getPort()
+        {
+            foreach (string s in SerialPort.GetPortNames())
+            {
+                comboBox1.Items.Add(s);
+            }
+        }
+        //----------------------------------------------------------  Обработчик события загрузки формы, потути наш main
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            getPort();      //Выводим в комбобокс доступные порты
+            if (comboBox1.Items.Count > 0) comboBox1.SelectedIndex = 0;     //Если они есть, выводим первый
+
+            progressBar1.Minimum = 0;                                       //Настройки прогресс бара
+            progressBar1.Maximum = 0x1FFFFF;
+            progressBar1.Value = 0;
+
+            // Chart
+            //Задание маштаба по оси X
+            //chart1.ChartAreas[0].AxisX.ScaleView.Zoom(0, 50);   //При старте от показаны будут точки только от 0 до 50
+            //Включим возможность использования курсора по оси X
+            chart1.ChartAreas[0].CursorX.IsUserEnabled = true;
+            //Включим возможность мастабирования по выделенному интервалу
+            chart1.ChartAreas[0].CursorX.IsUserSelectionEnabled = true;
+            //Включим мастабирование по оси X
+            chart1.ChartAreas[0].AxisX.ScaleView.Zoomable = true;
+            //Добавим полосу прокрутки
+            chart1.ChartAreas[0].AxisX.ScrollBar.IsPositionedInside = true;
+            chart1.ChartAreas[0].CursorX.Interval = 0.01;
+
+
+
+            //Задание маштаба по оси Y
+            chart1.ChartAreas[0].AxisY.ScaleView.Zoom(90000, 105000);   //При старте от показаны будут точки только от 0 до 50
+            //Включим возможность использования курсора по оси Y
+            chart1.ChartAreas[0].CursorY.IsUserEnabled = true;
+            //Включим возможность мастабирования по выделенному интервалу
+            chart1.ChartAreas[0].CursorY.IsUserSelectionEnabled = true;
+            //Включим мастабирование по оси Y
+            chart1.ChartAreas[0].AxisY.ScaleView.Zoomable = true;
+            //Добавим полосу прокрутки
+            chart1.ChartAreas[0].AxisY.ScrollBar.IsPositionedInside = true;
+
+
+            //chart2.ChartAreas[0].AxisX.ScaleView.Zoomable = true;
+            //chart2.ChartAreas[0].AxisY.ScaleView.Zoomable = true;
+            
+            }
+        //---------------------------------------------------------  Обработчик нажатия кнопки
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int selectedIndex = comboBox1.SelectedIndex;
+            Object selectedItem = comboBox1.SelectedItem;
+
+            Thread readThread = new Thread(Read);
+
+            // Create a new SerialPort object with default settings.
+            _serialPort = new SerialPort();
+
+            // Allow the user to set the appropriate properties.
+            _serialPort.PortName = selectedItem.ToString();
+            _serialPort.BaudRate = 115200;
+            _serialPort.Parity = (Parity)Enum.Parse(typeof(Parity), "None", true);
+            _serialPort.DataBits = 8;
+            _serialPort.StopBits = (StopBits)Enum.Parse(typeof(StopBits), "One", true);
+            _serialPort.Handshake = (Handshake)Enum.Parse(typeof(Handshake), "None", true);
+
+            // Set the read/write timeouts
+            _serialPort.ReadTimeout = 500;
+            _serialPort.WriteTimeout = 500;
+
+            _serialPort.Open();
+            _continue = true;
+            readThread.Start();
+        }
+        //-------------------------------------------------------------------------------------
+        private void DisplayText(object sender, EventArgs e)
+        {
+            progressBar1.Value = uart_count;
+        }
+        //-------------------------------------------------------------------------------------
+        private void saveData()
+        {
+            using (BinaryWriter writer = new BinaryWriter(File.Open(fileName, FileMode.Create)))
+            {
+                for (int i = 0; i < 0x1FFFFF; i++)
+                {
+                    writer.Write(uart_mass[i]);
+                }
+                writer.Write(true);
+                writer.Close();
+            }
+
+            const string message = "Данные записаны и сохранены";
+            const string caption = "Результат";
+            MessageBox.Show(message, caption);
+        }
+        //-------------------------------------------------------------------------------------
+        public void Read()    //Поток чтения из COM-port
+        {
+            while (_continue)
+            {
+                try
+                {
+                    int n_uart = _serialPort.BytesToRead;
+                    if (n_uart > 0)
+                    {
+                        _serialPort.Read(uart_mass, uart_count, n_uart);
+                        uart_count = uart_count + n_uart;
+                        this.Invoke(new EventHandler(DisplayText));
+                        if (uart_count == 0x1FFFFF) saveData();
+                    }
+                }
+                catch (TimeoutException) { Application.Exit(); }
+            }
+        }
+        //-----------------------------------------------------------  Кнопка считать данные
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+
+            saveFileDialog1.Filter = "dat files (*.dat)|*.dat|All files (*.*)|*.*";
+            saveFileDialog1.FilterIndex = 1;
+            saveFileDialog1.RestoreDirectory = true;
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                fileName = saveFileDialog1.FileName;
+                for (int i = 0; i < 0x1FFFFF; i++) uart_mass[i] = 0xFF;
+                uart_count = 0;
+                if (_continue)
+                {
+                    _serialPort.Write("1\r");
+                }
+            }
+            
+            //-------------------------------------------------------------------------------------
+
+        }
+        //--------------Вывод данных их файла в массив ----
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //Из файла
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.Filter = "dat files (*.dat)|*.dat|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 1;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                fileName = openFileDialog1.FileName;
+
+                if (File.Exists(fileName))
+                {
+                    using (BinaryReader reader = new BinaryReader(File.Open(fileName, FileMode.Open)))
+                    {
+                        uart_mass = reader.ReadBytes(0x1FFFFF);
+                    }
+                }
+            }
+
+
+        }
+        //-------------------------------------------------------------  Филитр
+        private long filter(float val)
+        {  //функция фильтрации
+            Pc = P + varProcess;
+            G = Pc / (Pc + varVario);
+            P = (1 - G) * Pc;
+            Xp = Xe;
+            Zp = Xp;
+            Xe = G * (val - Zp) + Xp; // "фильтрованное" значение
+            return ((long)Xe);
+        }
+        //-----------------------------------------------------------  Вывод давления
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int findPoint;
+
+            // Поиск завершения реальных данных
+            for (findPoint=0; findPoint<0x1FFFF0; findPoint = findPoint + 4)
+            {
+                if ((uart_mass[findPoint] == 0xFF) && (uart_mass[findPoint + 1] == 0xFF))
+                    break;
+            }
+
+            //Выисление длиныы массива для сырых данных от датчика
+            int len16bit = (findPoint)/4;
+
+            chart1.Series[0].Points.Clear();
+
+            
+            for (long w = 0; w < len16bit; w++)
+            {
+                float timSec = 0.105f * w;
+                uint daw = uart_mass[(w * 4)];
+                daw = daw << 8;
+                daw = daw | uart_mass[(w * 4) + 1];
+                daw = daw << 8;
+                daw = daw | uart_mass[(w * 4) + 2];
+                daw = daw << 8;
+                daw = daw | uart_mass[(w * 4) + 3]; 
+                float filtered = filter(daw);
+                chart1.Series[0].Points.AddXY(timSec, daw);
+                chart1.Series[1].Points.AddXY(timSec, filtered);
+                if (w == 0)
+                {
+                    ex_r = filtered;
+                    chart2.Series[0].Points.AddXY(timSec, 0);
+                }
+                else
+                {
+                    float del = filtered - ex_r;
+                    ex_r = filtered;
+                    chart2.Series[0].Points.AddXY(timSec, del);
+                }
+                
+                
+            }
+
+        }
+        //-----------------------------------------------------------
+        private void button5_Click(object sender, EventArgs e)
+        {
+            float P1 = float.Parse(textBox1.Text);
+            float P2 = float.Parse(textBox2.Text);
+            double H = 18400 * (1 + 0.003665f) * Math.Log10(P1 / P2);
+            textBox3.Text = string.Format("{0:0.##}", H) + " м";
+            float T1 = float.Parse(textBox4.Text);
+            float T2 = float.Parse(textBox5.Text);
+            float T3 = T2 - T1;
+            float S = (float)H / T3;
+            textBox6.Text = string.Format("{0:0.##}", S)+" м/с";
+            textBox7.Text = string.Format("{0:0.##}", T3) + " с";
+        }
+
+        private void chart1_AxisViewChanged(object sender, System.Windows.Forms.DataVisualization.Charting.ViewEventArgs e)
+        {
+            //
+            double minX = chart1.ChartAreas[0].AxisX.ScaleView.ViewMinimum;
+            double maxX = chart1.ChartAreas[0].AxisX.ScaleView.ViewMaximum;
+            //chart2.ChartAreas[0].AxisX.ScaleView.Zoom(minX, maxX);
+
+            double minY = chart1.ChartAreas[0].AxisY.ScaleView.ViewMinimum;
+            double maxY = chart1.ChartAreas[0].AxisY.ScaleView.ViewMaximum;
+            //chart2.ChartAreas[0].AxisY.ScaleView.Zoom(minY, maxY);
+
+            //chart1.ChartAreas[0].AxisX.
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.F1)
+            {
+                textBox1.Text = chart1.ChartAreas[0].CursorY.Position.ToString();
+                textBox4.Text = chart1.ChartAreas[0].CursorX.Position.ToString();
+            }
+            if (e.KeyCode == Keys.F2)
+            {
+                textBox2.Text = chart1.ChartAreas[0].CursorY.Position.ToString();
+                textBox5.Text = chart1.ChartAreas[0].CursorX.Position.ToString();
+
+                float P1 = float.Parse(textBox1.Text);
+                float P2 = float.Parse(textBox2.Text);
+                double H = 18400 * (1 + 0.003665f) * Math.Log10(P1 / P2);
+                textBox3.Text = string.Format("{0:0.##}", H) + " м";
+                float T1 = float.Parse(textBox4.Text);
+                float T2 = float.Parse(textBox5.Text);
+                float T3 = T2 - T1;
+                float S = (float)H / T3;
+                textBox6.Text = string.Format("{0:0.##}", S) + " м/с";
+                textBox7.Text = string.Format("{0:0.##}", T3) + " с";
+            }
+        }
+
+        private void textBox2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.F1)
+            {
+                textBox1.Text = chart1.ChartAreas[0].CursorY.Position.ToString();
+                textBox4.Text = chart1.ChartAreas[0].CursorX.Position.ToString();
+            }
+            if (e.KeyCode == Keys.F2)
+            {
+                textBox2.Text = chart1.ChartAreas[0].CursorY.Position.ToString();
+                textBox5.Text = chart1.ChartAreas[0].CursorX.Position.ToString();
+
+                float P1 = float.Parse(textBox1.Text);
+                float P2 = float.Parse(textBox2.Text);
+                double H = 18400 * (1 + 0.003665f) * Math.Log10(P1 / P2);
+                textBox3.Text = string.Format("{0:0.##}", H) + " м";
+                float T1 = float.Parse(textBox4.Text);
+                float T2 = float.Parse(textBox5.Text);
+                float T3 = T2 - T1;
+                float S = (float)H / T3;
+                textBox6.Text = string.Format("{0:0.##}", S) + " м/с";
+                textBox7.Text = string.Format("{0:0.##}", T3) + " с";
+            }
+        }
+    }
+}
